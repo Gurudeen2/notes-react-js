5ed35d46115ca33fca75dc27d20343d7222e7200
ControlPlus <akeemtolanifatai@gmail.com>
2023-03-02 11:59:55 +0100

    new commit

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -25,35 +25,7 @@ const NewTask = (props) => {
       createTask
     );
   };
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       'https://movito10-default-rtdb.firebaseio.com/tasks.json',
-  //       {
-  //         method: 'POST',
-  //         body: JSON.stringify({ text: taskText }),
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //       }
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error('Request failed!');
-  //     }
-
-  //     const data = await response.json();
-
-  //     const generatedId = data.name; // firebase-specific => "name" contains generated id
-  //     const createdTask = { id: generatedId, text: taskText };
-
-  //     props.onAddTask(createdTask);
-  //   } catch (err) {
-  //     setError(err.message || 'Something went wrong!');
-  //   }
-  //   setIsLoading(false);
-  // };
+ 
 
   return (
     <Section>
